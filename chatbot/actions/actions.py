e648875ecaa7519494b97834d266d02c358900af
sebastienguimety <seb.guimety@gmail.com>
2023-11-18 17:19:00 +0100

    feat: :sparkles: Schedule
    New schedule form, student ( alternant, classique )

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -7,21 +7,43 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ActionSaySchedule(Action):
+
+    def name(self) -> Text:
+        return "action_say_schedule"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        section = tracker.get_slot("section")
+        group = tracker.get_slot("group")
+        
+        if not section:
+            dispatcher.utter_message(text="I don't know your section.")
+        elif not group:
+            dispatcher.utter_message(text="I don't know your group.")
+        else:
+            dispatcher.utter_message(text=f"Your section is {section} and your group is {group}!")
+       
+        return []
+    
