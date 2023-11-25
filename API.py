f4066d6d4970fd2fb29df45590edf6732f07f9ae
sebastienguimety <seb.guimety@gmail.com>
2023-11-25 13:00:56 +0100

    wip: Generate email with partage API

---
+++ API.py
@@ -0,0 +1,21 @@
+import requests
+from urllib import parse
+import hashlib
+
+
+def php_md5(text: str) -> str:
+    result = hashlib.md5(text.encode())
+    print(result.hexdigest())
+    return result.hexdigest()
+
+
+url = "https://api-dosi-test.univ-avignon.fr/partage/v1/Token/getToken"
+payload = {
+    'user': 'uapv1901981',
+    'passClient': php_md5('Sebgavoite84340_')
+}
+
+r = requests.post(url, data=payload)
+print(r.text)
+
+
--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -8,25 +8,15 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 import requests
-
+import os
 from typing import Any, Text, Dict, List
+from actions.api.demo_actions import PartageZimbraCom
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from CeriBot.PartagePegado.demo import *
+import sys
+print("OONN  EESTTTT OOUUUUUUUUUUUUUU")
 
-
-class ActionHelloWorld(Action):
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
 
 class ActionSaySchedule(Action):
 
